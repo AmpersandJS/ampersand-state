b4dca7d154f038d4a7ce7194233e434838862ca3
Henrik Joreteg <hjoreteg@gmail.com>
2014-02-18 13:10:36 -0800

    more tests

--- test/basics.js
+++ test/basics.js
@@ -1,85 +1,54 @@
 var Statey = require('../statey');
 
 
-module.exports = {
-    setUp: function (cb) {
-        this.Person = Statey.extend({
-            props: {
-                name: 'string'
-            }
-        });
-
-        this.AwesomePerson = this.Person.extend({
-            props: {
-                awesomeness: ['number', true, 7]
-            },
-            awesomeMethod: function () {
-                return this.awesomeness;
-            }
-        });
-
-        this.SuperHero = this.AwesomePerson.extend({
-            props: {
-                secretName: 'string'
-            },
-            fly: function () {
-                return 'i\'m sad i\'m flying';
-            }
-        });
-        cb();
-    },
-    'init with values': function (test) {
-        var person = new this.Person({name: 'henrik'});
-        test.ok(person);
-        test.equal(person.name, 'henrik');
-        test.done();
-    },
-    'derived properties': function (test) {
-        var count = 0;
-        var Person = this.Person.extend({
-            derived: {
-                greeting: {
-                    deps: ['name'],
-                    fn: function () {
-                        count++;
-                        return 'hi, ' + this.name + '!';
-                    }
-                }
-            }
-        });
-        var person = new Person({name: 'henrik'});
-        test.equal(person.greeting, 'hi, henrik!');
-
-        // use again, should not increment counter
-        person.greeting;
-        test.equal(count, 1);
-
-        person.name = 'something';
-        test.equal(person.greeting, 'hi, something!');
-        // reference again
-        person.greeting;
-        test.equal(count, 2);
-
-        test.done();
-    },
-    'cached derived events fire on dependency change': function (test) {
-        var Person = this.Person.extend({
-            derived: {
-                greeting: {
-                    deps: ['name'],
-                    fn: function () {
-                        return 'hi, ' + this.name + '!';
-                    }
-                }
-            }
-        });
-        var person = new Person({name: 'henrik'});
-        person.on('change:greeting', function (model, value) {
-            test.equal(value, 'hi, something!')
-            test.done();
-        });
-
-        person.name = 'something';
-    }
+exports.setUp = function (cb) {
+    this.Person = Statey.extend({
+        props: {
+            name: 'string'
+        }
+    });
+    cb();
 };
 
+exports['init with values'] = function (test) {
+    var person = new this.Person({name: 'henrik'});
+    test.ok(person);
+    test.equal(person.name, 'henrik');
+    test.done();
+};
+
+exports['extended object maintains existing props'] = function (test) {
+    var AwesomePerson = this.Person.extend({
+        props: {
+            awesomeness: 'number'
+        }
+    });
+
+    var awesome = new AwesomePerson({
+        name: 'Captain Awesome',
+        awesomeness: 11
+    });
+
+    test.equals(awesome.name, 'Captain Awesome');
+    test.equals(awesome.awesomeness, 11);
+    test.done();
+};
+
+exports['extended object maintains existing methods'] = function (test) {
+    var Person = Statey.extend({
+        props: {
+            awesomeness: 'number'
+        },
+        isTrulyAwesome: function () {
+            if (this.awesomeness > 10) return true;
+        }
+    });
+    var AwesomePerson = Person.extend({});
+
+    var awesome = new AwesomePerson({
+        awesomeness: 11
+    });
+
+    test.ok(awesome.isTrulyAwesome());
+    test.done();
+};
