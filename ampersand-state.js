3f6f21a7ef1b53fb7f8485d4d4582f0926236677
Michael Garvin <gar@andyet.net>
2014-08-15 16:19:04 -0700

    Throw error on invalid extraProperties
    See issue #10

--- ampersand-state.js
+++ ampersand-state.js
@@ -149,6 +149,8 @@ _.extend(Base.prototype, BBEvents, {
                     throw new TypeError('No "' + attr + '" property defined on ' + (this.type || 'this') + ' model and extraProperties not set to "ignore" or "allow"');
                 } else if (extraProperties === 'allow') {
                     def = this._createPropertyDefinition(attr, 'any');
+                } else if (extraProperties) {
+                    throw new TypeError('Invalid value for "extraProperties: ' + extraProperties);
                 }
             }
 
